6e3fbfdd0617921bf40f7beea2e68b021cf7f06f
FelixKrall <94911325+FelixKrall@users.noreply.github.com>
2022-02-27 16:15:44 +0100

    Functions1.R
    Ich hab die dev.off() in die Grafikfunktionen geschrieben und das load zu source geändert.

--- Functions1.R
+++ Functions1.R
@@ -1,3 +1,4 @@
+
 #Hier die Funktionen zur Auswertung
 test <- data.frame("alter"=round(rnorm(100, mean = 25,sd = 2),1),
                    "fach"=factor(sample(c("A", "B", "C"),100, rep=T)),
@@ -8,70 +9,90 @@ test <- data.frame("alter"=round(rnorm(100, mean = 25,sd = 2),1),
 # (a) Eine Funktion, die verschiedene geeignete deskriptive Statistiken
 # fuer metrische Variablen berechnet und ausgibt
 
-metric <- function(v, boxpl = FALSE){
-  if(boxpl){boxplot(v)}
-  return(c(summary(v), "Var" = var(v)))
+metric <- function(v, name, boxpl=FALSE){
+  if(boxpl){
+    dev.off()
+    boxplot(v, xlab=name, main=paste("Auswertung", name))
+    }
+  return(c(summary(v), "Varianz"=var(v)))
 }
 #Test
-metric(test$alter)
+metric(test$alter, "Alter")
+
+
 
 # b) Eine Funktion, die verschiedene geeignete deskriptive Statistiken
-# fuer kategoriale Variablen berechnet und ausgibt
+# fuer kategoriale Variablen berechnet und ausgibt.
+# Erstellt den Modus, die absolute Haeufigkeit und die relative Haeufigkeit.
 
-kateg <- function(v, graf = FALSE){
+kateg <- function(v, name, plot=TRUE){
+  source("Functions2.R")
+  tab <- table(v)
+  mod <- mod(v)
   
   #Grafik
-  if(graf){
-    n <- length(levels(v))
+  if(plot){
+    dev.off()
+    n <- length(levels(as.factor(v)))
     maH <- max(table(v))
     plot.new()
-    plot.window(xlim=c(0,n+1), ylim=c(0,maH))
-    axis(1, at=0:(n+1), labels = c("",levels(v),""))
+    plot.window(xlim=c(0,n+1), ylim=c(0,maH*1.3))
+    par(oma=c(3,3,3,3))
+    axis(1, at=0:(n+1), labels = c("",1:n,""))
     axis(2)
     rect(xleft=(1:n)-0.5, ybottom=0, xright=(1:n)+0.5, ytop=table(v))
+    mtext(text=paste("Haeufigkeiten", name),side = 3, outer=TRUE)
+    mtext("Auspraegung", 1, outer = TRUE)
+    mtext("Haeufigkeit", 2, outer = TRUE)
   }
-  return(list("Modus" = names(which(table(v)==max(table(v)))), "Hauefigkeiten" = table(v),
-              "relative Hauefigkeiten" = table(v)*(1/length(v))))
-
+  return(list("Modus"=mod, "abs. Haeufigkeiten"=tab, "rel. Haeufigkeiten"=tab*(1/length(v))))
 }
 #Test
-kateg(test$IntMath, TRUE)
+kateg(test$IntMath, "Interesse Mathe")
 
 # (c) Eine Funktion, die geeignete deskriptive bivariate Statistiken fuer
 # den Zusammenhang zwischen zwei kategorialen Variablen
-# berechnet ausgibt
+# berechnet ausgibt.
+# Erstellt die gemeinsame Haeufigkeit und die dazugehoerige relative 
+# Haeufigkeit.
 
-bivKateg <- function(v1, v2){
-  par(mfrow=c(1,2))
-  
+bivKateg <- function(v1, v2, name1, name2, plot){
   #einzelne Auswertungen
-  cat("Auswewrtung der Variable v1", "\n")
-  print(kateg(v1))
+  cat("Auswertung der Variable v1", "\n")
+  print(kateg(v1,name1))
   cat("--------------------------", "\n")
   cat("Auswertung der Variable v2", "\n")
-  print(kateg(v2))
+  print(kateg(v2,name2))
   cat("--------------------------", "\n")
   
   #gemeinsame Haeufigkeiten
-  haeufigkeitstabelle = table(v1, v2)
-  rel_haeufigkeitstabelle = (1/min(c(length(v1), length(v2))))*haeufigkeitstabelle
-  return(list("Kombinationshauefigkeiten" = haeufigkeitstabelle, 
-              "relative Kombinationshaeufigkeiten" = rel_haeufigkeitstabelle))
+  tab <- table(v1, v2)
+  rel_tab <- (1/min(c(length(v1), length(v2))))*tab
   
   #evtl noch Plot der gemeinsamen Haeufigkeiten
+  if(plot){
+    dev.off()
+    plot(table(v1,v2), xlab=name1, ylab=name2, 
+         main=paste("Zusammenhang", name1 ,"und", name2))
+  }
+  
+  return(list("gemeinsame Haeufigkeiten"= tab, "rel. Haeufigkeiten"=rel_tab))
+  
 }
 
 #Test
-bivKateg(test$IntMath, test$MLK)
+bivKateg(test$IntMath, test$MLK, "Interesse Mathe", "Mathe LK", TRUE)
 
 
 # (d) Eine Funktion, die geeignete deskriptive bivariate Statistiken fuer
 # den Zusammengang zwischen einer metrischen und einer
 # dichotomen Variablen berechnet und ausgibt
 
-bivMetDicho <- function(met, dic){
-  boxplot(met ~ dic)
-  realisationen_von_dic = dic[which(duplicated(janein) == FALSE)]
+bivMetDicho <- function(met, dic, nameMet, nameDic){
+  dev.off()
+  boxplot(met ~ dic, xlab=nameDic, ylab=nameMet, 
+          main=paste("Zusammenhang", nameMet, "und", nameDic))
+  realisationen_von_dic = dic[which(duplicated(dic) == FALSE)]
   met1 = met[which(dic == realisationen_von_dic[1])]
   met2 = met[which(dic == realisationen_von_dic[2])]
   
@@ -80,41 +101,55 @@ bivMetDicho <- function(met, dic){
 }
 
 #Test
-bivMetDicho(met = test$alter, dic=test$MLK)
+bivMetDicho(met = test$alter, dic= test$MLK, "Alter", "MatheLK")
 
 
 # (e) Eine Funktion, die eine mindestens ordinal skalierte Variable
 # quantilbasiert kategorisiert (z.B. in niedrig, mittel, hoch)
 
-#unterteilt in hoch, mittel oder niedrig
-#Hoch heißt beispielswese, dass 66% der Realisierungen kleiner sind, als die untere 
-#Grenze des Intervals für hoch
 quantKateg <- function(v){
-  v = as.numeric(v)
-  quants = quantile(v, c(0.33,0.66))
-  categ = 0
+  quantiles <- quantile(v, c(0.33,0.66))
+  kat <- 0
   for(i in 1:length(v)){
-    if(v[i] <= quants[1]){categ[i] = "niedirg"}
-    if(v[i] <= quants[2] && v[i] > quants[1]){categ[i] = "mittel"} 
-    if(v[i] > quants[2]){categ[i] = "hoch"}
-    }
-  
-  return(list("Qunatile von v" = quants, "Kategorisierter_Vektor" = categ))
-
+    if(v[i]<=quantiles[1]){kat[i] <- "niedrig"}
+    if(v[i]<=quantiles[2] && v[i]>quantiles[1]){kat[i] <- "mittel"}
+    if(v[i]>quantiles[2]){kat[i] <- "hoch"}
+  }
+  return(kat)
 }
+
 #Test
-quantKateg(test$IntMath)
+quantKateg(test$alter)
 
 
 
 # (f) Eine Funktion, die eine geeignete Visualisierung von drei oder vier
 # kategorialen Variablen erstellt
 
-visualize <- function(){
-  
+visualize <- function(v1, name1, v2, name2, v3, name3, v4 = "", name4 = "") {
+  par(mfrow = c(2, 2), mar = c(4, 6, 2, 1))
+  barplot(table(v1), horiz = TRUE, las = 1, col = "lightblue", xlim = c(0, max(table(v1))*1.3),
+          main=name1)
+  box()
+  mtext(text = "Haeufigkeit", side = 1, line = 2.2)
+  barplot(table(v2), horiz = TRUE, las = 1, col = "red4", xlim = c(0, max(table(v2))*1.3),
+          main=name2)
+  box()
+  mtext(text = "Haeufigkeit", side = 1, line = 2.2)
+  barplot(table(v3), horiz = TRUE, las = 1, col = "green4", xlim = c(0, max(table(v3))*1.3),
+          main=name3)
+  box()
+  mtext(text = "Haeufigkeit", side = 1, line = 2.2)
+  if (v4 != "") {
+    barplot(table(v4), horiz = TRUE, las = 1, col = "darkviolet", xlim = c(0, max(table(v4))*1.3),
+            main=name4)
+    box()
+    mtext(text = "Haeufigkeit", side = 1, line = 2.2)
+  }
 }
 
-
+visualize(v1=test$fach, name1="Fach", v2=test$IntMath, name2="InteresseMathe", v3=test$IntPro,
+          name3="Interesse Programmieren", v4=test$MLK, name4="MatheLK")
 
 # Freiwillig: weitere zur Deskription und Visualisierung geeignete
 # Funktionen
